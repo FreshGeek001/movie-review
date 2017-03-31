b7547d35d14ee3a25a99177fad470b36419c68b4
FreshGeek001 <memory2026@126.com>
2017-03-31 14:38:13 +0800

    add permission check

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,6 @@
 class MoviesController < ApplicationController
-  before_action :authenticate_user!, only:[:new, :create]
+  before_action :authenticate_user!, only:[:new, :create, :edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit,:update,:destroy]
   def index
     @movies = Movie.all
   end
@@ -23,11 +24,10 @@ class MoviesController < ApplicationController
   end
 
   def edit
-    @movie = Movie.find(params[:id])
   end
 
   def update
-    @movie = Movie.find(params[:id])
+
     if @movie.update(movie_params)
       redirect_to movies_path
     else
@@ -36,12 +36,20 @@ class MoviesController < ApplicationController
   end
 
   def destroy
-    @movie = Movie.find(params[:id])
+  
     @movie.destroy
     redirect_to movies_path, alert:"Movie Deleted"
   end
 
   private
+
+  def find_group_and_check_permission
+    @movie = Movie.find(params[:id])
+
+    if current_user != @movie.user
+      redirect_to root_path, alert:"You have no permission"
+    end
+  end
 
   def movie_params
     params.require(:movie).permit(:title, :description)
