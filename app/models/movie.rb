38c68b0efa42ec0177fe1effcfc263ef978a9cde
FreshGeek001 <memory2026@126.com>
2017-04-01 14:47:20 +0800

    add post and hook group/user relation

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,5 @@ class Movie < ApplicationRecord
   validates :title, presence: true
   belongs_to :user
   mount_uploader :image, ImageUploader
+  has_many :posts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170401024737) do
+ActiveRecord::Schema.define(version: 20170401064545) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20170401024737) do
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
     t.string   "image"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "movie_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
