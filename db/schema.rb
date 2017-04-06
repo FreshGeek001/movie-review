7e00ed7386000f807717f954fd6a28ef362e6191
FreshGeek001 <memory2026@126.com>
2017-04-06 15:12:40 +0800

    add ratyrate to movie

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170401074950) do
+ActiveRecord::Schema.define(version: 20170406070235) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer  "movie_id"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20170401074950) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "rating"
   end
 
   create_table "users", force: :cascade do |t|
